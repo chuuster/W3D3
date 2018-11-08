f39f82f1ea786ab89c9f8a0915823137c321951a
chuuster <chu.christina@yahoo.com>
2018-11-07 17:52:53 -0800

    Add associations to ShortenedUrl

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -8,6 +8,11 @@ class ShortenedUrl < ApplicationRecord
     foreign_key: :user_id,
     class_name: :User
 
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :url_id,
+    class_name: :Visit
+
   def self.random_code
     url = SecureRandom.urlsafe_base64
     while exists?(url)
@@ -20,5 +25,20 @@ class ShortenedUrl < ApplicationRecord
     ShortenedUrl.create!(short_url: "coolurls.co/#{ShortenedUrl.random_code}", long_url: long_url, user_id: user.id)
   end
 
+  def num_clicks
+    self.visits.count
+  end
+
+  def num_uniques
+    self.visits.select(:user_id).map { |v| v.user_id }.uniq.count
+  end
+
+  def num_recent_uniques
+    self.visits.select(:user_id).where("created_at - NOW() < make_interval(mins := 10)").map { |v| v.user_id }.uniq.count
+  end
+
+  def self.[](index)
+    ShortenedUrl.all[index]
+  end
 
 end
--- URLShortener/app/models/user.rb
+++ URLShortener/app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :ShortenedUrl
+
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Visit
+
+  has_many :visited_urls,
+    through: :visits,
+    source: :user
+
+  def self.[](index)
+    User.all[index]
+  end
 end
